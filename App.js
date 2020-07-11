62f1a318af3062ee3c7d0911a270d752354596bd
Pranav <vaidpranav89@gmail.com>
2020-07-11 12:44:36 +0100

    added camera and location

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import MyCamera from './screens/Camera';
+import  LocationData,{text} from './screens/MapView';
+
 
 import NavigationContainer from './navigation/tabsNavigation';
 
@@ -7,5 +10,6 @@ import NavigationContainer from './navigation/tabsNavigation';
 
 export default function App() {
   return (
-    <NavigationContainer/>    
+    <LocationData/> 
+      
   )};
